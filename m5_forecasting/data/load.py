020ae166a47bb6fff121cdcde5dec1093cc4ce86
Teppei Kanayama <teppei@Kanayama-2.local>
2020-04-03 17:34:10 +0900

    data load

---
+++ m5_forecasting/data/load.py
@@ -0,0 +1,17 @@
+from logging import getLogger
+
+import gokart
+
+logger = getLogger(__name__)
+
+
+class LoadInputData(gokart.TaskOnKart):
+    task_namespace = 'm5-forecasting'
+
+    def output(self):
+        calender = self.make_target('input/calendar.csv', use_unique_id=False)
+        sales_train_validation = self.make_target('input/sales_train_validation.csv', use_unique_id=False)
+        sample_submission = self.make_target('input/sample_submission_accuracy.csv', use_unique_id=False)
+        sell_prices = self.make_target('input/sell_prices.csv', use_unique_id=False)
+        return dict(calender=calender, sales_train_validation=sales_train_validation,
+                    sample_submission=sample_submission, sell_prices=sell_prices)
--- m5_forecasting/data/preprocess.py
+++ m5_forecasting/data/preprocess.py
@@ -7,14 +7,8 @@ from m5_forecasting.data.load import LoadInputData
 logger = getLogger(__name__)
 
 
-class Sample(gokart.TaskOnKart):
+class PreprocessInputData(gokart.TaskOnKart):
     task_namespace = 'm5-forecasting'
-
-    # def output(self):
-    #     return self.make_target('data/sample.pkl')
-    #
-    # def run(self):
-    #     self.dump('sample output')
 
     def requires(self):
         return LoadInputData()
