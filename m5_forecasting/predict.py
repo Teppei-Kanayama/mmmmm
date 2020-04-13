b62a5427cba513496c9a34709c9ab595026d1dc6
金山　哲平 <teppei.kanayama2@m3-2019mac02.local>
2020-04-13 12:10:11 +0900

    0.59405(再現)

--- m5_forecasting/predict.py
+++ m5_forecasting/predict.py
@@ -5,14 +5,12 @@ from typing import List
 import gokart
 import luigi
 import pandas as pd
-import numpy as np
 from lightgbm import Booster
 
 from m5_forecasting.data.calendar import PreprocessCalendar
 from m5_forecasting.data.feature_engineering import MergeData, MakeFeature
 from m5_forecasting.data.sales import PreprocessSales, MekeSalesFeature
 from m5_forecasting.data.selling_price import PreprocessSellingPrice
-from m5_forecasting.data.load import LoadInputData
 from m5_forecasting.train import Train
 
 logger = getLogger(__name__)
@@ -66,11 +64,9 @@ class Predict(gokart.TaskOnKart):
 
     is_small: bool = luigi.BoolParameter()
 
-    from_date: int = luigi.IntParameter()  # 1914, 1915, ,,,
-    to_date: int = luigi.IntParameter()  # 1915, 1916, ,,,
+    from_date: int = luigi.IntParameter()
+    to_date: int = luigi.IntParameter()
     interval: int = luigi.IntParameter()
-    # latest_prediction_task: gokart.TaskOnKart = gokart.TaskInstanceParameter()
-    # trained_model_task: gokart.TaskOnKart = gokart.TaskInstanceParameter()
 
     def output(self):
         return self.make_target(os.path.join('predict', f'predict_{self.from_date}_{self.to_date}.csv'),
@@ -82,10 +78,6 @@ class Predict(gokart.TaskOnKart):
 
         calendar_data_task = PreprocessCalendar()
         selling_price_data_task = PreprocessSellingPrice()
-
-        # sales_data_task = PreprocessSales(is_small=self.is_small) if 'is_dummy' in self.latest_prediction_task.param_kwargs.keys() \
-        #     else self.latest_prediction_task
-
         sales_data_task = PreprocessSales(is_small=self.is_small)
         predicted_sales_data_task = ConcatPredictionData(from_date=VALIDATION_START_DATE, to_date=self.from_date,
                                                          interval=self.interval)
@@ -113,48 +105,5 @@ class Predict(gokart.TaskOnKart):
         pred = model.predict(test[feature_columns])
         sales.loc[sales[(sales['id'].isin(test['id'])) & (sales['d'].isin(test['d']))].index, 'demand'] = pred
         return sales[(predict_from_date <= sales['d']) & (sales['d'] < predict_to_date)]
-
-
-# class PredictAll(gokart.TaskOnKart):
-#     task_namespace = 'm5-forecasting'
-#
-#     interval: int = luigi.IntParameter()
-#
-#     def output(self):
-#         return self.make_target('submission.csv')
-#
-#     def requires(self):
-#         assert 28 % self.interval == 0, 'interval is invalid!'
-#
-#         model_task = Train()
-#         assert model_task.input()['model'].exists(), "trained model doesn't exists!"
-#
-#         def make_prediction(from_date, interval):
-#             previous_prediction = DummyTask() if from_date == VALIDATION_START_DATE else make_prediction(from_date - interval, interval)
-#             return Predict(predict_from_date=from_date, predict_to_date=from_date + interval,
-#                            trained_model_task=model_task, latest_prediction_task=previous_prediction)
-#
-#         predict = make_prediction(EVALUATION_START_DATE - self.interval, self.interval)
-#         sample_submission_data_task = LoadInputData(filename='sample_submission.csv')
-#         return dict(predict=predict, sample_submission=sample_submission_data_task)
-#
-#     def run(self):
-#         test = self.load_data_frame('predict')
-#         sample_submission = self.load_data_frame('sample_submission')
-#         output = self._run(test, sample_submission)
-#         self.dump(output)
-#
-#     @staticmethod
-#     def _run(test, sample_submission):
-#         test = test.fillna(-1)  # evaluation scores
-#         test = test.assign(id=test.id + "_" + np.where(test.d < EVALUATION_START_DATE, "validation", "evaluation"),
-#                            F="F" + (test.d - VALIDATION_START_DATE + 1
-#                                     - DURATION * (test.d >= EVALUATION_START_DATE)).astype("str"))
-#         submission = test.pivot(index="id", columns="F", values="demand").reset_index()[sample_submission.columns]
-#         return submission
-
-
-# python main.py m5-forecasting.PredictAll --interval 7 --local-scheduler
-# DATA_SIZE=small python main.py m5-forecasting.PredictAll --interval 7 --local-scheduler
 
 # DATA_SIZE=small python main.py m5-forecasting.Predict --from-date 1914 --to-date 1915 --interval 1 --local-scheduler
