ef02118dec0d94f67ac3188424267c3750ed4012
金山　哲平 <teppei.kanayama2@m3-2019mac02.local>
2020-04-13 12:14:44 +0900

    argv

--- script/predict_batch.py
+++ script/predict_batch.py
@@ -1,9 +1,11 @@
 import subprocess
+import sys
 
 
 def main():
     subprocess.run(['python', 'main.py', 'm5-forecasting.Train', '--local-scheduler'])
-    interval = 7
+
+    interval = int(sys.argv[1])
     for t in range(1914, 1942, interval):
         subprocess.run(['python', 'main.py', 'm5-forecasting.Predict', f'--from-date={t}', f'--to-date={t+interval}',
                         f'--interval={interval}', '--local-scheduler'])
@@ -12,3 +14,6 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# python script/predict_batch.py 7
+# python script/predict_batch.py 1
