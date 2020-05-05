cfcd6e9975dd481eb049dc687e8b5ed7f911c6c4
金山　哲平 <teppei.kanayama2@m3-2019mac02.local>
2020-05-05 22:39:07 +0900

    refactor done

--- m5_forecasting/predict_uncertainty.py
+++ m5_forecasting/predict_uncertainty.py
@@ -1,18 +1,19 @@
+from typing import Optional, List
+
 import gokart
 import luigi
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
 
-from m5_forecasting import Submit
 from m5_forecasting.data.load import LoadInputData
 
 
 PERCENTILES = np.array([0.005, 0.025, 0.165, 0.25, 0.5, 0.75, 0.835, 0.975, 0.995])
 
 LEVELS = ["id", "item_id", "dept_id", "cat_id", "store_id", "state_id", "_all_"]
-COUPLES = [("state_id", "item_id"), ("state_id", "dept_id"), ("store_id", "dept_id"), ("state_id", "cat_id"),
-           ("store_id", "cat_id")]
+COUPLES = [["state_id", "item_id"], ["state_id", "dept_id"], ["store_id", "dept_id"], ["state_id", "cat_id"],
+           ["store_id", "cat_id"]]
 COLS = [f"F{i}" for i in range(1, 29)]
 
 
@@ -42,11 +43,7 @@ class PredictUncertainty(gokart.TaskOnKart):
         ratios = cls._transform_qs_to_ratio(PERCENTILES)
         sub = accuracy.merge(sales[["id", "item_id", "dept_id", "cat_id", "store_id", "state_id"]], on="id")
         sub["_all_"] = "Total"
-        df_list = []
-        for level in LEVELS:
-            df_list.append(cls.get_group_preds(ratios, sub, level))
-        for level1, level2 in COUPLES:
-            df_list.append(cls.get_couple_group_preds(ratios, sub, level1, level2))
+        df_list = [cls._calculate_uncertainty(sub, ratios, levels) for levels in (LEVELS + COUPLES)]
         df = pd.concat(df_list, axis=0, sort=False).reset_index(drop=True)
         return cls._make_submission(df)
 
@@ -59,43 +56,27 @@ class PredictUncertainty(gokart.TaskOnKart):
         return ratios
 
     @staticmethod
+    def _calculate_uncertainty(point_prediction: pd.DataFrame, ratios: pd.Series, levels):
+        df = point_prediction.groupby(levels)[COLS].sum()
+        q = np.repeat(ratios.index, len(df))  # [0.005, ,,, 0.005, ,,, 0.995, ,,, ,0.995]
+        df = pd.concat([df] * 9, axis=0, sort=False).reset_index()
+        df[COLS] *= ratios.loc[q].values[:, None]  # 点予測の値をuncertaintyの予測に変換する # ここが一番重要
+
+        if type(levels) == list:
+            df["id"] = [f"{lev1}_{lev2}_{q:.3f}_validation" for lev1, lev2, q in zip(df[levels[0]].values, df[levels[1]].values, q)]
+        elif levels != "id":
+            df["id"] = [f"{lev}_X_{q:.3f}_validation" for lev, q in zip(df[levels].values, q)]
+        else:
+            df["id"] = [f"{lev.replace('_validation', '')}_{q:.3f}_validation" for lev, q in zip(df[levels].values, q)]
+        df = df[["id"] + COLS]
+        return df
+
+    @staticmethod
     def _make_submission(df: pd.DataFrame) -> pd.DataFrame:
         df = pd.concat([df, df], axis=0, sort=False)
         df.reset_index(drop=True, inplace=True)
         df.loc[df.index >= len(df.index) // 2, "id"] = df.loc[df.index >= len(df.index) // 2, "id"].str.replace(
             "_validation$", "_evaluation")
-        return df
-
-    @staticmethod
-    def get_group_preds(ratios, pred, level):
-        def quantile_coefs(q):
-            return ratios.loc[q].values
-
-        df = pred.groupby(level)[COLS].sum()
-        q = np.repeat(ratios.index, len(df))
-        df = pd.concat([df] * 9, axis=0, sort=False)
-        df.reset_index(inplace=True)
-        df[COLS] *= quantile_coefs(q)[:, None]
-        if level != "id":
-            df["id"] = [f"{lev}_X_{q:.3f}_validation" for lev, q in zip(df[level].values, q)]
-        else:
-            df["id"] = [f"{lev.replace('_validation', '')}_{q:.3f}_validation" for lev, q in zip(df[level].values, q)]
-        df = df[["id"] + list(COLS)]
-        return df
-
-    @staticmethod
-    def get_couple_group_preds(ratios, pred, level1, level2):
-        def quantile_coefs(q):
-            return ratios.loc[q].values
-
-        df = pred.groupby([level1, level2])[COLS].sum()
-        q = np.repeat(ratios.index, len(df))
-        df = pd.concat([df] * 9, axis=0, sort=False)
-        df.reset_index(inplace=True)
-        df[COLS] *= quantile_coefs(q)[:, None]
-        df["id"] = [f"{lev1}_{lev2}_{q:.3f}_validation" for lev1, lev2, q in
-                    zip(df[level1].values, df[level2].values, q)]
-        df = df[["id"] + list(COLS)]
         return df
 
 
