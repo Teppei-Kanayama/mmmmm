42e293ea084b6a513dafa6dcf24f6d34ece9a0cc
金山　哲平 <teppei.kanayama2@m3-2019mac02.local>
2020-05-16 17:11:36 +0900

    refactor

--- m5_forecasting/adversarial_validation/adversarial_validation.py
+++ m5_forecasting/adversarial_validation/adversarial_validation.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import gokart
 import luigi
 import pandas as pd
@@ -14,38 +16,31 @@ from m5_forecasting.data.selling_price import PreprocessSellingPrice
 class TrainBinaryLGBM(gokart.TaskOnKart):
     task_namespace = 'm5-forecasting'
 
-    def requires(self):
-        pass
+    feature_task = gokart.TaskInstanceParameter()
+    target_term: Tuple = luigi.ListParameter(default=[1914 - 365, 1914 - 1])
+    source_term: Tuple = luigi.ListParameter()
 
-    def run(self):
-        pass
-
-
-class AdversarialValidation(gokart.TaskOnKart):
-    task_namespace = 'm5-forecasting'
-
-    is_small: bool = luigi.BoolParameter()
+    def output(self):
+        model = self.make_target(relative_file_path='adversarial_validation/lgb.pkl')
+        feature_columns = self.make_target(relative_file_path='adversarial_validation/feature_columns.pkl')
+        feature_importance = self.make_target(relative_file_path='adversarial_validation/feature_importance.csv')
+        return dict(model=model, feature_columns=feature_columns, feature_importance=feature_importance)
 
     def requires(self):
-        calendar_data_task = PreprocessCalendar()
-        selling_price_data_task = PreprocessSellingPrice()
-        sales_data_task = PreprocessSales(is_small=self.is_small)
-        merged_data_task = MergeData(calendar_data_task=calendar_data_task,
-                                     selling_price_data_task=selling_price_data_task, sales_data_task=sales_data_task)
-        feature_task = MakeFeature(merged_data_task=merged_data_task)
-        return dict(feature=feature_task)
+        return self.feature_task
 
     def run(self):
-        feature = self.load_data_frame('feature')
-        feature_columns = [feature_column for feature_column in feature.columns if feature_column not in ['id', 'd', 'year', 'week_of_year', 'day']]
+        feature = self.load_data_frame()
+        feature_columns = [feature_column for feature_column in feature.columns if
+                           feature_column not in ['id', 'd', 'year', 'week_of_year', 'day']]
 
-        target_term = (1914 - 365, 1914 - 1)
-        source_term = (1914 - 365 * 2, 1914 - 365 - 1)
+        # target_term = (1914 - 365, 1914 - 1)
+        # source_term = (1914 - 365 * 2, 1914 - 365 - 1)
 
-        target_feature = feature[feature['d'].between(*target_term)]
-        source_feature = feature[feature['d'].between(*source_term)]
-
+        target_feature = feature[feature['d'].between(*self.target_term)]
         target_feature['target'] = 1
+
+        source_feature = feature[feature['d'].between(*self.source_term)]
         source_feature['target'] = 0
 
         data = pd.concat([target_feature, source_feature])
@@ -60,28 +55,50 @@ class AdversarialValidation(gokart.TaskOnKart):
         val_set = lgb.Dataset(X_test, y_test)
 
         # TODO: change parameters!
-        params = {"objective": "binary", "metric": "auc", "force_row_wise": True, "learning_rate": 0.075,
-                  "sub_row": 0.75, "bagging_freq": 1, "lambda_l2": 0.1, 'verbosity': 1}
-
+        params = {"objective": "binary", "metric": "auc", "force_row_wise": True, "learning_rate": 0.075, "sub_row": 0.75,
+                  "bagging_freq": 1, "lambda_l2": 0.1, 'verbosity': 1}
         num_boost_round = 1000
         early_stopping_rounds = 100
 
         valid_sets = [train_set, val_set]
-        model = lgb.train(params, train_set, num_boost_round=num_boost_round,
-                          early_stopping_rounds=early_stopping_rounds, valid_sets=valid_sets, verbose_eval=100)
+        model = lgb.train(params, train_set, num_boost_round=num_boost_round, early_stopping_rounds=early_stopping_rounds,
+                          valid_sets=valid_sets, verbose_eval=100)
 
         feature_importance = pd.DataFrame(
             dict(name=feature_columns, imp=model.feature_importance(importance_type='gain'))).sort_values(by='imp',
                                                                                                           ascending=False)
 
+        self.dump(model, 'model')
+        self.dump(feature_columns, 'feature_columns')
+        self.dump(feature_importance, 'feature_importance')
+
+
+class AdversarialValidation(gokart.TaskOnKart):
+    task_namespace = 'm5-forecasting'
+
+    is_small: bool = luigi.BoolParameter()
+
+    def output(self):
+        return self.make_target('adversarial_validation/result.csv')
+
+    def requires(self):
+        calendar_data_task = PreprocessCalendar()
+        selling_price_data_task = PreprocessSellingPrice()
+        sales_data_task = PreprocessSales(is_small=self.is_small)
+        merged_data_task = MergeData(calendar_data_task=calendar_data_task,
+                                     selling_price_data_task=selling_price_data_task, sales_data_task=sales_data_task)
+        feature_task = MakeFeature(merged_data_task=merged_data_task)
+        train_tasks = [TrainBinaryLGBM(feature_task=feature_task, source_term=[1914 - 365 * (i + 1), 1914 - 365 * i - 1]) for i in range(1, 5)]
+        return train_tasks
+
+    def run(self):
+        model_list = self.load()
+        import pdb; pdb.set_trace()
+
         y_hat_test = model.predict(X_test)
 
         fpr, tpr, thresholds = metrics.roc_curve(y_test + 1, y_hat_test, pos_label=2)
         score = metrics.auc(fpr, tpr)
-        import pdb; pdb.set_trace()
-        self.dump(model, 'model')
-        self.dump(feature_columns, 'feature_columns')
-        self.dump(feature_importance, 'feature_importance')
 
 
 # DATA_SIZE=small python main.py m5-forecasting.Adversarial
