4e1158d98b52f16d87f2702f26aa66303c81daf5
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-05-18 11:54:43 +0900

    to class

--- m5_forecasting/metric/wrmsse.py
+++ m5_forecasting/metric/wrmsse.py
@@ -3,34 +3,30 @@ import pandas as pd
 from scipy.sparse import csr_matrix
 
 
-# Function to do quick rollups:
-def rollup(v, roll_mat_csr):
-    '''
-    v - np.array of size (30490 rows, n day columns)
-    v_rolledup - array of size (n, 42840)
-    '''
-    return roll_mat_csr * v  # (v.T*roll_mat_csr.T).T
+class WRMSSECalculator:
+    def __init__(self, s, w, sw, roll_mat_csr) -> None:
+        self._s = s
+        self._w = w
+        self._sw = sw,
+        self._roll_mat_csr = roll_mat_csr
 
-
-# Function to calculate WRMSSE:
-def wrmsse(preds, y_true, s, w, sw, roll_mat_csr, score_only=False):
-    '''
-    preds - Predictions: pd.DataFrame of size (30490 rows, N day columns)
-    y_true - True values: pd.DataFrame of size (30490 rows, N day columns)
-    sequence_length - np.array of size (42840,)
-    sales_weight - sales weights based on last 28 days: np.array (42840,)
-    '''
-
-    if score_only:
+    def calculate_scores(self, y_hat, y_gt):
         return np.sum(
             np.sqrt(
                 np.mean(
-                    np.square(rollup(preds.values - y_true.values, roll_mat_csr))
-                    , axis=1)) * sw) / 12  # <-used to be mistake here
-    else:
-        score_matrix = (np.square(rollup(preds.values - y_true.values, roll_mat_csr)) * np.square(w)[:, None]) / s[:, None]
+                    np.square(self._rollup(y_hat.values - y_gt.values))
+                    , axis=1)) * self._sw) / 12
+
+    def calculate_scores_with_matrix(self, y_hat, y_gt):
+        score_matrix = (np.square(self._rollup(y_hat.values - y_gt.values)) * np.square(self._w)[:, None]) / self._s[:, None]
         score = np.sum(np.sqrt(np.mean(score_matrix, axis=1))) / 12  # <-used to be mistake here
         return score, score_matrix
+
+    # Function to do quick rollups:
+    def _rollup(self, v):
+        # v - np.array of size (30490 rows, n day columns)
+        # v_rolledup - array of size (n, 42840)
+        return self._roll_mat_csr * v  # (v.T*roll_mat_csr.T).T
 
 
 def main():
@@ -48,6 +44,8 @@ def main():
     roll_mat_csr = csr_matrix(roll_mat_df.values)
     del roll_mat_df
 
+    calculator = WRMSSECalculator(s=S, w=W, sw=SW, roll_mat_csr=roll_mat_csr)
+
     # Predictions:
     sub = pd.read_csv(file_pass + 'sample_submission_accuracy.csv')
     sales = pd.read_csv(file_pass + 'sales_train_validation.csv')
@@ -60,10 +58,10 @@ def main():
     dayCols = ["d_{}".format(i) for i in range(1914 - DAYS_PRED, 1914)]
     y_true = sales[dayCols]
 
-    score = wrmsse(sub, y_true, S, W, SW, roll_mat_csr, score_only=True)
+    score = calculator.calculate_scores(sub, y_true)
+    x, y = calculator.calculate_scores_with_matrix(sub, y_true)
 
-
-    import pdb; pdb.set_trace()
+    print(score, x, y)
 
 
 if __name__ == '__main__':
