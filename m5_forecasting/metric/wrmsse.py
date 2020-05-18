3d19201a720e3331cc9c4231023fe6e265bb463f
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-05-18 12:36:44 +0900

    debug

--- m5_forecasting/metric/wrmsse.py
+++ m5_forecasting/metric/wrmsse.py
@@ -6,16 +6,21 @@ from scipy.sparse import csr_matrix
 
 
 class WRMSSECalculator:
-    def __init__(self, s: np.ndarray, w: np.ndarray, sw: np.ndarray, roll_mat_csr: csr_matrix) -> None:
+    def __init__(self, s: np.ndarray, w: np.ndarray, sw: np.ndarray, roll_mat_csr: csr_matrix, sample_submission: pd.DataFrame) -> None:
         self._s = s
         self._w = w
         self._sw = sw,
         self._roll_mat_csr = roll_mat_csr
+        self._sample_submission = sample_submission
 
     def calculate_scores(self, y_hat: pd.DataFrame, y_gt: pd.DataFrame) -> float:
+        assert 'id' in y_hat.columns
+        assert 'id' in y_gt.columns
+
         # y_hat, y_gt: ID数x日数のDataFrame
-        # y_hat.shape == y_gt.shape == (30490, 28)
-        # IDの順序はsumple submissionと同じである必要あり
+        # y_hat.shape == y_gt.shape == (30490, 28+1)
+        y_hat = pd.merge(self._sample_submission[['id']], y_hat).drop('id', axis=1)
+        y_gt = pd.merge(self._sample_submission[['id']], y_gt).drop('id', axis=1)
         return np.sum(
             np.sqrt(
                 np.mean(
@@ -37,6 +42,17 @@ class WRMSSECalculator:
 def main():
     file_pass = 'resources/input/'
 
+    # sample submission
+    ss = pd.read_csv(file_pass + 'sample_submission_accuracy.csv')
+
+    # Predictions:
+    sub = pd.read_csv(file_pass + 'emsembled.csv')
+    sub = sub[sub.id.str.endswith('validation')]
+
+    # Psudo Ground truth:
+    ground_truth = pd.read_csv('resources/kernel/kkiller_first_public_notebook_under050_v5.csv')
+    ground_truth = ground_truth[ground_truth.id.str.endswith('validation')]
+
     # Load S and W weights for WRMSSE calcualtions:
     sw_df = pd.read_pickle(file_pass + 'sw_df.pkl')
     S = sw_df.s.values
@@ -48,24 +64,12 @@ def main():
     roll_mat_csr = csr_matrix(roll_mat_df.values)
     del roll_mat_df
 
-    calculator = WRMSSECalculator(s=S, w=W, sw=SW, roll_mat_csr=roll_mat_csr)
+    calculator = WRMSSECalculator(s=S, w=W, sw=SW, roll_mat_csr=roll_mat_csr, sample_submission=ss)
 
-    # Predictions:
-    sub = pd.read_csv(file_pass + 'sample_submission_accuracy.csv')
-    sales = pd.read_csv(file_pass + 'sales_train_validation.csv')
-    sub = sub[sub.id.str.endswith('validation')]
-    sub.drop(['id'], axis=1, inplace=True)
+    score = calculator.calculate_scores(sub, ground_truth)
+    # x, y = calculator.calculate_scores_with_matrix(sub, ground_truth)
 
-    DAYS_PRED = sub.shape[1]  # 28
-
-    # Ground truth:
-    dayCols = ["d_{}".format(i) for i in range(1914 - DAYS_PRED, 1914)]
-    y_true = sales[dayCols]
-
-    score = calculator.calculate_scores(sub, y_true)
-    x, y = calculator.calculate_scores_with_matrix(sub, y_true)
-
-    print(score, x, y)
+    print(score)  # 0.196
 
 
 if __name__ == '__main__':
