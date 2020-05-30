3fc710845fb4d0e52f56aada308a9f40afd10642
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-05-30 13:31:04 +0900

    add mean encoding feature

--- m5_forecasting/data/feature_engineering.py
+++ m5_forecasting/data/feature_engineering.py
@@ -53,21 +53,23 @@ class MakeFeature(gokart.TaskOnKart):
         feature1_task = LoadInputData(filename='grid_part_1.pkl')
         feature2_task = LoadInputData(filename='grid_part_2.pkl')
         feature3_task = LoadInputData(filename='grid_part_3.pkl')
+        mean_encoding_feature_task = LoadInputData(filename='small_mean_encoding_feature.pkl')
         return dict(data=self.merged_data_task, feature1=feature1_task, feature2=feature2_task,
-                    feature3=feature3_task)
+                    feature3=feature3_task, mean_encoding_feature=mean_encoding_feature_task)
 
     def run(self):
         data = self.load_data_frame('data')
         feature1 = self.load('feature1')
         feature2 = self.load('feature2')
         feature3 = self.load('feature3')
-        output = self._run(data, feature1, feature2, feature3)
+        mean_encoding_feature = self.load('mean_encoding_feature')
+        output = self._run(data, feature1, feature2, feature3, mean_encoding_feature)
         self.dump(output)
 
     @classmethod
-    def _run(cls, data, feature1, feature2, feature3):
+    def _run(cls, data, feature1, feature2, feature3, mean_encoding_feature):
         data = cls._label_encode(data)
-        data = cls._merge_outside_feature(data, feature1, feature2, feature3)
+        data = cls._merge_outside_feature(data, feature1, feature2, feature3, mean_encoding_feature)
         return data
 
     @staticmethod
@@ -77,22 +79,32 @@ class MakeFeature(gokart.TaskOnKart):
         return data
 
     @staticmethod
-    def _merge_outside_feature(data, feature1, feature2, feature3):
-        # データのフォーマットを合わせる
+    def _merge_outside_feature(data, feature1, feature2, feature3, mean_encoding_feature):
+        logger.info('merge mean_encoding_feature ...')
+        mean_encoding_feature['id'] = mean_encoding_feature['id'].apply(lambda x: x.split('_validation')[0])
+        data = pd.merge(data, mean_encoding_feature, on=['id', 'd'], how='inner')
+        del mean_encoding_feature
+
+        logger.info('merge feature1 ...')
         feature1['id'] = feature1['id'].apply(lambda x: x.split('_validation')[0])
-        feature2['id'] = feature2['id'].apply(lambda x: x.split('_validation')[0])
-        feature3['id'] = feature3['id'].apply(lambda x: x.split('_validation')[0])
-        feature2['d'] = feature2['d'].apply(lambda x: int(x.split('_')[1]))
-        feature3['d'] = feature3['d'].apply(lambda x: int(x.split('_')[1]))
-
-        # 必要なカラムを取り出す
         feature1_columns = ['id', 'd', 'release']
-        feature2_columns = ['id', 'd', 'price_max', 'price_min', 'price_std', 'price_mean', 'price_norm', 'price_nunique', 'item_nunique',
-                            'price_momentum', 'price_momentum_m', 'price_momentum_y']
-        feature3_columns = ['id', 'd', 'tm_d', 'tm_w', 'tm_m', 'tm_y', 'tm_wm', 'tm_dw', 'tm_w_end']
-
-        # mergeする
         data = pd.merge(data, feature1[feature1_columns], on=['id', 'd'], how='inner')
+        del feature1
+
+        logger.info('merge feature2 ...')
+        feature2['id'] = feature2['id'].apply(lambda x: x.split('_validation')[0])
+        feature2['d'] = feature2['d'].apply(lambda x: int(x.split('_')[1]))
+        feature2_columns = ['id', 'd', 'price_max', 'price_min', 'price_std', 'price_mean', 'price_norm',
+                            'price_nunique', 'item_nunique',
+                            'price_momentum', 'price_momentum_m', 'price_momentum_y']
         data = pd.merge(data, feature2[feature2_columns], on=['id', 'd'], how='inner')
+        del feature2
+
+        logger.info('merge feature3 ...')
+        feature3['id'] = feature3['id'].apply(lambda x: x.split('_validation')[0])
+        feature3['d'] = feature3['d'].apply(lambda x: int(x.split('_')[1]))
+        feature3_columns = ['id', 'd', 'tm_d', 'tm_w', 'tm_m', 'tm_y', 'tm_wm', 'tm_dw', 'tm_w_end']
         data = pd.merge(data, feature3[feature3_columns], on=['id', 'd'], how='inner')
+        del feature3
+
         return data
