68bd961dcdf9b5ada94eaed815e93cc709aaf24e
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-05-30 13:39:50 +0900

    batch

--- script_batch/upload_input_files.sh
+++ script_batch/upload_input_files.sh
@@ -2,6 +2,7 @@
 
 set -euC
 
+aws s3 cp resources/input/small_mean_encoding_feature.pkl s3://kaggle-m5-filestore/input/
 aws s3 cp resources/input/calendar.csv s3://kaggle-m5-filestore/input/
 aws s3 cp resources/input/sales_train_validation.csv s3://kaggle-m5-filestore/input/
 aws s3 cp resources/input/sample_submission.csv s3://kaggle-m5-filestore/input/
@@ -12,4 +13,3 @@ aws s3 cp resources/input/sell_prices.csv s3://kaggle-m5-filestore/input/
 aws s3 cp resources/input/grid_part_1.pkl s3://kaggle-m5-filestore/input/
 aws s3 cp resources/input/grid_part_2.pkl s3://kaggle-m5-filestore/input/
 aws s3 cp resources/input/grid_part_3.pkl s3://kaggle-m5-filestore/input/
-
