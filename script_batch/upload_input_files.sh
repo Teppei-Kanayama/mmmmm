201da67e54db4c45ecd1fca9e8cf80416fdbfda4
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-06-03 09:19:54 +0900

    evaluation was good :)

--- script_batch/upload_input_files.sh
+++ script_batch/upload_input_files.sh
@@ -2,14 +2,15 @@
 
 set -euC
 
-aws s3 cp resources/input/small_mean_encoding_feature.pkl s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/calendar.csv s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/sales_train_validation.csv s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/sample_submission.csv s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/sample_submission_uncertainty.csv s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/sell_prices.csv s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/weights_validation.csv s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/sell_prices.csv s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/grid_part_1.pkl s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/grid_part_2.pkl s3://kaggle-m5-filestore/input/
-aws s3 cp resources/input/grid_part_3.pkl s3://kaggle-m5-filestore/input/
+aws s3 cp resources/input/sales_train_evaluation.csv s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/small_mean_encoding_feature.pkl s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/calendar.csv s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/sales_train_validation.csv s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/sample_submission.csv s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/sample_submission_uncertainty.csv s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/sell_prices.csv s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/weights_validation.csv s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/sell_prices.csv s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/grid_part_1.pkl s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/grid_part_2.pkl s3://kaggle-m5-filestore/input/
+#aws s3 cp resources/input/grid_part_3.pkl s3://kaggle-m5-filestore/input/
