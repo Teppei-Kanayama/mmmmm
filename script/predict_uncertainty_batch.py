a24b132a5872c82deb7e1290e98cb9f032726562
Teppei Kanayama <kanayama.teppei@gmail.com>
2020-06-22 17:41:30 +0900

    refactor uncertainty

--- script/predict_uncertainty_batch.py
+++ script/predict_uncertainty_batch.py
@@ -2,22 +2,29 @@ import subprocess
 import sys
 
 
-def main():
-    variance_duration = 10  # TODO: もっと伸ばしてみる
-    variance_from_date = 1914 - 7 * variance_duration
-    variance_to_date = 1914
-    interval = int(sys.argv[1])
-
-    # pointwise
-    subprocess.run(['python', 'main.py', 'm5-forecasting.TrainPointwiseModel', f'--train-to-date={variance_from_date}', '--local-scheduler'])
-    for t in range(variance_from_date, variance_to_date, interval):
+def run_pointwise_model(train_to_date, validate_from_date, validate_to_date, interval):
+    subprocess.run(['python', 'main.py', 'm5-forecasting.TrainPointwiseModel', f'--train-to-date={train_to_date}', '--local-scheduler'])
+    for t in range(validate_from_date, validate_to_date, interval):
         subprocess.run(
             ['python', 'main.py', 'm5-forecasting.PredictPointwise',
-             f'--prediction-start-date={variance_from_date}',
+             f'--train-to-date={train_to_date}',
+             f'--prediction-start-date={validate_from_date}',
              f'--predict-from-date={t}',
              f'--predict-to-date={t + interval}',
              f'--interval={interval}',
              '--local-scheduler'])
+
+
+def main():
+    variance_duration = 10
+    variance_to_date = 1914  # TODO: 1942
+    variance_from_date = variance_to_date - 7 * variance_duration
+    interval = int(sys.argv[1])
+
+    run_pointwise_model(train_to_date=variance_from_date,
+                        validate_from_date=variance_from_date,
+                        validate_to_date=variance_to_date,
+                        interval=interval)
 
     # uncertainty
     subprocess.run(
@@ -33,4 +40,3 @@ if __name__ == '__main__':
 
 # DATA_SIZE=small python script/predict_uncertainty_batch.py 7
 # python script/predict_uncertainty_batch.py 7
-
